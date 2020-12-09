404253b75d9ad1bcbdbced320f51fb7174c8c6e2
Nicolas@DESKTOP-K9DI919 <nicolasfuentes@hotmail.com>
2020-12-09 14:14:55 +0100

    handle route path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/movies/popular',
-        pathMatch: 'full'
-    },
-    {
-        path: 'movies/:category',
-        loadChildren: () => import('./movies-list/movies.module').then(m => m.MoviesModule)
-    },
-    {
-        path: 'movies/:category/:movieId',
-        component: MovieComponent
-    }
+    { path: 'movies/:category', loadChildren: () => import('./movies-list/movies.module').then(m => m.MoviesModule) },
+    { path: 'movies/:category/:movieId', component: MovieComponent },
+    { path: '**', redirectTo: '/movies/popular', pathMatch: 'full' }
 ]
 
 @NgModule({
